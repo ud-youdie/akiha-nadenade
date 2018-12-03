edc33e5aa38cabf03b887eb90e990f4d0835d80c
ud-youdie <ud.grandspring@gmail.com>
2018-12-03 23:53:54 +0900

    ローディング画面を追加

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,40 @@
 $(function(){
 
+    $("#waiting").css({
+        "position": "fixed",
+        "top": 0,
+        "left": 0,
+        "width": "100%",
+        "height": "100%",
+        "text-align": "center",
+        "background-color": "#ffffff"
+    });
+
     const imgPath = "./src/img/akiha_{0}.png";
     const frameCount = 35;
+    var loadCounter = 0;
 
     for (let i = 0; i <= frameCount; i++) {
         var id = ('000' + i).slice( -3 );
         var img = $("<img />").attr({
             "id": id,
             "src": imgPath.replace("{0}", id)
-        });
-        $("#view").append(img);
+        }).one("load",()=>{
+            loadCounter += 1;
+            $("#count").text(loadCounter);
+        }).appendTo("#view");
     }
 
     var $images = $("#view").children();
     $images.hide();
     $images.eq(0).show();
+
+    var itv = setInterval(()=>{
+        if (frameCount < loadCounter) {
+            clearInterval(itv);
+            $("#waiting").hide();
+        }
+    }, 10);
 
     $("#range").on("input", ()=>{
         var val = $("#range").val();
